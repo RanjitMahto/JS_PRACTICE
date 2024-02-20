db786457f8ff91a0f5351840e7402ff224477bcd
RanjitMahto <116587259+RanjitMahto@users.noreply.github.com>
2024-02-20 21:08:38 +0000

    variables and dataypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict" //treat all js code as newer verssion
+
+//alert(3+3)  //we are using nodejs, not browser... so it will give error in this
+
+
+ console.log(3+
+    3);  // code readability should be high 
+ console.log("ranjit")
+
+
+ let name ="ranjit"
+ let age = 22
+ let isLoggedIn = false
+
+
+ // number :- 2 to power 53
+ // bigint:-
+ //string:-""
+ //boolean:- true/false
+ //null:- standalone value
+ //undefined:-when u do not define value
+//symbol:-
+
+
+//typeof:- for checking datatype
+console.log(typeof isLoggedIn); 
---
+++ 01_basics/variables.js
@@ -0,0 +1,21 @@
+const accountId =1223
+let accountEmail = "ranjit@googlr.com"
+var accountPassword = "122"
+accountCity = "Bokaro"
+let accountState;
+
+
+// accountId =122   " not allowed"
+
+/*
+prefer not to  use  var
+because of issue in block scope and functional scope
+
+*/
+
+accountEmail="rah@c.com"
+accountPassword="344"
+accountCity ="chandigarh"
+
+// Consol.table:-for creating table (output in the form of index and values)
+console.table([accountId,accountCity,accountEmail,accountPassword,accountState]); 

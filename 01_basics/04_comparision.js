ae6e94527242e66d9e165258fae97a675f52c3b9
RanjitMahto <116587259+RanjitMahto@users.noreply.github.com>
2024-02-21 09:43:07 +0000

    comparision operations

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+
+//console.log("2">1)  // firstly "2" is converted into number then comapare so it will true
+//console.log("02a" >1)
+
+//console.log(null>0)  // null firstly converted to 0 then check 0>0= false
+//console.log(null == 0) // false
+//console.log(null >= 0)  // true
+
+console.log(undefined == 0)
+console.log(undefined >0)
+console.log(undefined <0)
+console.log(undefined !=0)
+
+console.log("2" == 2)
+
+
+// strict equality check === :- it also check data types
+
+console.log("2" === 2)
+

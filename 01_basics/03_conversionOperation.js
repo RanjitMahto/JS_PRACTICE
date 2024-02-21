b7df52b5feee8bc192eaf68c6565dc75863556e8
RanjitMahto <116587259+RanjitMahto@users.noreply.github.com>
2024-02-21 09:21:51 +0000

    operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score =true
+//let score =true
 // let score=22;
 // let score="22"
 // let score  = "22ab"
@@ -27,9 +27,65 @@ let score =true
 //""= false
 // 0= false
 
-let someNumber = 33
+//let someNumber = 33
 //console.log(typeof someNumber);
-let stringNumber= String(someNumber);
+//let stringNumber= String(someNumber);
 
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+//*********************** Operations ***************** */
+
+
+// let value = 3
+// let negVal= -value;
+// console.log(negVal);
+
+//console.log(2+2);
+//console.log(2*3);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+// let str1 = "hello"
+// let str2 =" Ranjit"
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log(1+2+"2");
+// 1 + 2: Here, 1 and 2 are both numbers, so JavaScript performs addition. The result of 1 + 2 is 3.
+// 3 + "2": After the first operation, you have 3 (a number) and "2" (a string). In JavaScript, when you use the + operator with a number and a string, the number is implicitly converted to a string, and then concatenation is performed.
+// So, 3 is converted to "3", and then "3" is concatenated with "2". The result is "32".
+// Therefore, when you run console.log(1 + 2 + "2"), the output will be "32".
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3= 2 +2   // say NOO to write this way
+console.log(num1);
+
+
+let gameCounter = 100
+++gameCounter;
+gameCounter--
+gameCounter++;
+console.log(gameCounter);
+
+let x = 3;
+const y = ++x;  // post increment
+// const y = ++x;  //pre increment
+console.log(x);
+console.log(y);
+// x is 4; y is 3
+
+let x2 = 3n;
+const y2 = x2++;
+console.log(x2);
+console.log(y2);
+// x2 is 4n; y2 is 3n

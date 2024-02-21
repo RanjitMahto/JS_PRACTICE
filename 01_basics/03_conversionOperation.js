8aa76806bb8ec698ec9aad04853c96a225b2cc27
RanjitMahto <116587259+RanjitMahto@users.noreply.github.com>
2024-02-21 08:42:01 +0000

    COnversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score =true
+// let score=22;
+// let score="22"
+// let score  = "22ab"
+// let score=null
+// let score="ranjit"
+
+
+// "33"=33
+//"22ab"=Nan
+// true= 1, false= 0
+
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+
+// let isLoggedIn= ""
+// let booleanIsLoggedIn= Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1= true
+//""= false
+// 0= false
+
+let someNumber = 33
+//console.log(typeof someNumber);
+let stringNumber= String(someNumber);
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
